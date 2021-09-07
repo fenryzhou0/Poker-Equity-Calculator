6dd92118eee5e9f2871d9d9a721cd3167de58c6c
fenryzhou0 <89749245+fenryzhou0@users.noreply.github.com>
2021-09-07 18:26:24 -0400

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,19 @@
+package com.company;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner hand1 = new Scanner;
+        System.out.println("Input the first hand: ");
+        Scanner hand2 = new Scanner;
+        System.out.println("Input the second hand: ");
+
+
+    }
+}
+
+class Cards {
+
+}
