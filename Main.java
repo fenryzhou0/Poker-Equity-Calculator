1115842f66351be77049a0c492663aae158560dd
fenryzhou0 <89749245+fenryzhou0@users.noreply.github.com>
2021-09-14 13:36:06 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,181 +3,286 @@ import java.util.*;
 
 public class Main {
 
-    static void combinationUtil(Object deck[], Object data[], int start, int end, int index, int r){
-        if (index == r) {
-            for (int j = 0; j < r; j++) {
-                System.out.print(data[j]+" ");
-            }
-            System.out.println("");
-
-            return;
-        }
-        for (int i = start; i <= end && end - i + 1 >= r - index; i++) {
-            data[index] = deck[i];
-            combinationUtil(deck, data, i + 1, end, index + 1, r);
-        }
-
-    }
-
-    static void printCombination(Object deck[], int n, int r) {
-        Object data[] = new Object[r];
-        combinationUtil(deck, data, 0, n-1, 0, r);
-    }
-
 
     public static void main(String[] args) {
-
+         final int[][] fullDeck = new int[][]{{2,1},{3,1},{4,1},{5,1},{6,1},{7,1},{8,1},{9,1},{10,1},{11,1},{12,1},{13,1},{14,1},{2,2},{3,2},{4,2},{5,2},{6,2},{7,2},{8,2},{9,2},{10,2},{11,2},{12,2},{13,2},{14,2},{2,3},{3,3},{4,3},{5,3},{6,3},{7,3},{8,3},{9,3},{10,3},{11,3},{12,3},{13,3},{14,3},{2,4},{3,4},{4,4},{5,4},{6,4},{7,4},{8,4},{9,4},{10,4},{11,4},{12,4},{13,4},{14,4}};
+         int[][] tempDeck = fullDeck;
+         //System.out.println(Arrays.deepToString(fullDeck));
          ArrayList<String> deckOfCards = new ArrayList<String>(); // something about this having to be a list and not array
          //String deck[] = new String [52]
 
+        Object[][] hand1 = new Object[2][2];
+        Object[][] hand2 = new Object[2][2];
+        int[][] hand1i = new int[2][2];
+        int[][] hand2i = new int[2][2];
 
-         String AceOfSpades = "As";
-         String AceOfClubs = "Ac";
-         String AceOfDiamonds = "Ad";
-         String AceOfHearts = "Ah";
-         String KingOfSpades = "Ks";
-         String KingOfClubs = "Kc";
-         String KingOfDiamonds = "Kd";
-         String KingOfHearts = "Kh";
-         String QueenOfSpades = "Qs";
-         String QueenOfClubs = "Qc";
-         String QueenOfDiamonds = "Qd";
-         String QueenOfHearts = "Qh";
-         String JackOfSpades = "Js";
-         String JackOfClubs = "Jc";
-         String JackOfDiamonds = "Jd";
-         String JackOfHearts = "Jh";
-         String TenofSpades = "Ts";
-         String TenofClubs = "Tc";
-         String TenOfDiamonds = "Td";
-         String TenOfHearts = "Th";
-         String NineOfSpades = "9s";
-         String NineOfClubs = "9c";
-         String NineOfDiamonds = "9d";
-         String NineOfHearts = "9h";
-         String EightOfSpades = "8s";
-         String EightOfClubs = "8c";
-         String EightOfDiamonds = "8d";
-         String EightOfHearts = "8h";
-         String SevenOfSpades = "7s";
-         String SevenOfClubs = "7c";
-         String SevenOfDiamonds = "7d";
-         String SevenOfHearts = "7h";
-         String SixOfSpades = "6s";
-         String SixOfClubs = "6c";
-         String SixOfDiamonds = "6d";
-         String SixOfHearts = "6h";
-         String FiveOfSpades = "5s";
-         String FiveOfClubs = "5c";
-         String FiveOfDiamonds = "5d";
-         String FiveOfHearts = "5h";
-         String FourOfSpades = "4s";
-         String FourOfClubs = "4c";
-         String FourOfDiamonds = "4d";
-         String FourOfHearts = "4h";
-         String ThreeOfSpades = "3s";
-         String ThreeOfClubs = "3c";
-         String ThreeOfDiamonds = "3d";
-         String ThreeOfHearts = "3h";
-         String TwoOfSpades = "2s";
-         String TwoOfClubs = "2c";
-         String TwoOfDiamonds = "2d";
-         String TwoOfHearts = "2h";
+        Scanner scan = new Scanner(System.in);
 
+        System.out.println("Input the first card of the first hand: ");
+            hand1[0] = scan.nextLine().split(" ");
 
-         deckOfCards.add(AceOfSpades);
-         deckOfCards.add(AceOfClubs);
-         deckOfCards.add(AceOfDiamonds);
-         deckOfCards.add(AceOfHearts);
-         deckOfCards.add(KingOfClubs);
-         deckOfCards.add(KingOfDiamonds);
-         deckOfCards.add(KingOfHearts);
-         deckOfCards.add(KingOfSpades);
-         deckOfCards.add(QueenOfClubs);
-         deckOfCards.add(QueenOfDiamonds);
-         deckOfCards.add(QueenOfHearts);
-         deckOfCards.add(QueenOfSpades);
-         deckOfCards.add(JackOfClubs);
-         deckOfCards.add(JackOfDiamonds);
-         deckOfCards.add(JackOfHearts);
-         deckOfCards.add(JackOfSpades);
-         deckOfCards.add(TenofClubs);
-         deckOfCards.add(TenOfDiamonds);
-         deckOfCards.add(TenOfHearts);
-         deckOfCards.add(TenofSpades);
-         deckOfCards.add(NineOfClubs);
-         deckOfCards.add(NineOfDiamonds);
-         deckOfCards.add(NineOfHearts);
-         deckOfCards.add(NineOfSpades);
-         deckOfCards.add(EightOfClubs);
-         deckOfCards.add(EightOfDiamonds);
-         deckOfCards.add(EightOfHearts);
-         deckOfCards.add(EightOfSpades);
-         deckOfCards.add(SevenOfClubs);
-         deckOfCards.add(SevenOfDiamonds);
-         deckOfCards.add(SevenOfHearts);
-         deckOfCards.add(SevenOfSpades);
-         deckOfCards.add(SixOfClubs);
-         deckOfCards.add(SixOfDiamonds);
-         deckOfCards.add(SixOfHearts);
-         deckOfCards.add(SixOfSpades);
-         deckOfCards.add(FiveOfClubs);
-         deckOfCards.add(FiveOfDiamonds);
-         deckOfCards.add(FiveOfHearts);
-         deckOfCards.add(FiveOfSpades);
-         deckOfCards.add(FourOfClubs);
-         deckOfCards.add(FourOfDiamonds);
-         deckOfCards.add(FourOfHearts);
-         deckOfCards.add(FourOfSpades);
-         deckOfCards.add(ThreeOfClubs);
-         deckOfCards.add(ThreeOfDiamonds);
-         deckOfCards.add(ThreeOfHearts);
-         deckOfCards.add(ThreeOfSpades);
-         deckOfCards.add(TwoOfClubs);
-         deckOfCards.add(TwoOfDiamonds);
-         deckOfCards.add(TwoOfHearts);
-         deckOfCards.add(TwoOfSpades);
-
-         Scanner hand1 = new Scanner(System.in);
-         System.out.println("Input the first card of the first hand: ");
-         String hand1card1 = hand1.nextLine();
-
-         Scanner hand1nextcard = new Scanner(System.in);
          System.out.println("Input the second card of the first hand: ");
-         String hand1card2 = hand1nextcard.nextLine();
+            hand1[1] = scan.nextLine().split(" ");
 
-         Scanner hand2 = new Scanner(System.in);
          System.out.println("Input the first card of the second hand: ");
-         String hand2card1 = hand2.nextLine();
+             hand2[0] = scan.nextLine().split(" ");
 
-         Scanner hand2nextcard = new Scanner(System.in);
          System.out.println("Input the second card of the second hand: ");
-         String hand2card2 = hand2nextcard.nextLine();
+             hand2[1] = scan.nextLine().split(" ");
 
-         for (int i = 0; i < deckOfCards.size(); i++) {
-             if (hand1card1.equals(deckOfCards.get(i))) {
-                 deckOfCards.remove(deckOfCards.get(i));
-             }
-             if (hand1card2.equals(deckOfCards.get(i))) {
-                 deckOfCards.remove(deckOfCards.get(i));
-             }
-             if (hand2card1.equals(deckOfCards.get(i))) {
-                 deckOfCards.remove(deckOfCards.get(i));
-             }
-             if (hand2card2.equals(deckOfCards.get(i))) {
-                 deckOfCards.remove(deckOfCards.get(i));
-             }
-
+         if(hand1[0][0] == "2" || hand1[0][0] == "3" || hand1[0][0] == "4" || hand1[0][0] == "5" || hand1[0][0] == "6" || hand1[0][0] == "7" || hand1[0][0] == "8" || hand1[0][0] == "9" || hand1[0][0] == "10")  {
+            hand1i[0][0] = (int) hand1[0][0];
+            if(hand1[0][1] == "Spade")  {
+                hand1i[0][1] = 1;
+            }   else if(hand1[0][1] == "Club")    {
+                hand1i[0][1] = 2;
+            }   else if(hand1[0][1] == "Heart")    {
+                hand1i[0][1] = 3;
+            }   else if(hand1[0][1] == "Diamond")    {
+                hand1i[0][1] = 4;
+            }
          }
+         else if(hand1[0][0] == "Jack") {
+             hand1i[0][0] = 11;
+             if(hand1[0][1] == "Spade")  {
+                 hand1i[0][1] = 1;
+             }   else if(hand1[0][1] == "Club")    {
+                 hand1i[0][1] = 2;
+             }   else if(hand1[0][1] == "Heart")    {
+                 hand1i[0][1] = 3;
+             }   else if(hand1[0][1] == "Diamond")    {
+                 hand1i[0][1] = 4;
+             }
+         }
+         else if(hand1[0][0] == "Queen") {
+             hand1i[0][0] = 12;
+             if(hand1[0][1] == "Spade")  {
+                 hand1i[0][1] = 1;
+             }   else if(hand1[0][1] == "Club")    {
+                 hand1i[0][1] = 2;
+             }   else if(hand1[0][1] == "Heart")    {
+                 hand1i[0][1] = 3;
+             }   else if(hand1[0][1] == "Diamond")    {
+                 hand1i[0][1] = 4;
+             }
+         }
+         else if(hand1[0][0] == "King") {
+             hand1i[0][0] = 13;
+             if(hand1[0][1] == "Spade")  {
+                 hand1i[0][1] = 1;
+             }   else if(hand1[0][1] == "Club")    {
+                 hand1i[0][1] = 2;
+             }   else if(hand1[0][1] == "Heart")    {
+                 hand1i[0][1] = 3;
+             }   else if(hand1[0][1] == "Diamond")    {
+                 hand1i[0][1] = 4;
+             }
+         }
+         else if(hand1[0][0] == "Ace") {
+             hand1i[0][0] = 1;
+             if(hand1[0][1] == "Spade")  {
+                 hand1i[0][1] = 1;
+             }   else if(hand1[0][1] == "Club")    {
+                 hand1i[0][1] = 2;
+             }   else if(hand1[0][1] == "Heart")    {
+                 hand1i[0][1] = 3;
+             }   else if(hand1[0][1] == "Diamond")    {
+                 hand1i[0][1] = 4;
+             }
+         }
+
+        if(hand1[1][0] == "2" || hand1[1][0] == "3" || hand1[1][0] == "4" || hand1[1][0] == "5" || hand1[1][0] == "6" || hand1[1][0] == "7" || hand1[1][0] == "8" || hand1[1][0] == "9" || hand1[1][0] == "10")  {
+            hand1i[1][0] = (int) hand1[1][0];
+            if(hand1[1][1] == "Spade")  {
+                hand1i[1][1] = 1;
+            }   else if(hand1[1][1] == "Club")    {
+                hand1i[1][1] = 2;
+            }   else if(hand1[1][1] == "Heart")    {
+                hand1i[1][1] = 3;
+            }   else if(hand1[1][1] == "Diamond")    {
+                hand1i[1][1] = 4;
+            }
+        }
+        else if(hand1[0][0] == "Jack")   {
+            hand1i[1][0] = 11;
+            if(hand1[1][1] == "Spade")  {
+                hand1i[1][1] = 1;
+            }   else if(hand1[1][1] == "Club")    {
+                hand1i[1][1] = 2;
+            }   else if(hand1[1][1] == "Heart")    {
+                hand1i[1][1] = 3;
+            }   else if(hand1[1][1] == "Diamond")    {
+                hand1i[1][1] = 4;
+            }
+        }
+        else if(hand1[0][0] == "Queen")   {
+            hand1i[1][0] = 12;
+            if(hand1[1][1] == "Spade")  {
+                hand1i[1][1] = 1;
+            }   else if(hand1[1][1] == "Club")    {
+                hand1i[1][1] = 2;
+            }   else if(hand1[1][1] == "Heart")    {
+                hand1i[1][1] = 3;
+            }   else if(hand1[1][1] == "Diamond")    {
+                hand1i[1][1] = 4;
+            }
+        }
+        else if(hand1[0][0] == "King")   {
+            hand1i[1][0] = 13;
+            if(hand1[1][1] == "Spade")  {
+                hand1i[1][1] = 1;
+            }   else if(hand1[1][1] == "Club")    {
+                hand1i[1][1] = 2;
+            }   else if(hand1[1][1] == "Heart")    {
+                hand1i[1][1] = 3;
+            }   else if(hand1[1][1] == "Diamond")    {
+                hand1i[1][1] = 4;
+            }
+        }
+        else if(hand1[0][0] == "Ace")   {
+            hand1i[1][0] = 1;
+            if(hand1[1][1] == "Spade")  {
+                hand1i[1][1] = 1;
+            }   else if(hand1[1][1] == "Club")    {
+                hand1i[1][1] = 2;
+            }   else if(hand1[1][1] == "Heart")    {
+                hand1i[1][1] = 3;
+            }   else if(hand1[1][1] == "Diamond")    {
+                hand1i[1][1] = 4;
+            }
+        }
+
+        if(hand2[0][0] == "2" || hand2[0][0] == "3" || hand2[0][0] == "4" || hand2[0][0] == "5" || hand2[0][0] == "6" || hand2[0][0] == "7" || hand2[0][0] == "8" || hand2[0][0] == "9" || hand2[0][0] == "10")  {
+            hand2i[0][0] = (int) hand2[0][0];
+            if(hand2[0][1] == "Spade")  {
+                hand2i[0][1] = 1;
+            }   else if(hand2[0][1] == "Club")    {
+                hand2i[0][1] = 2;
+            }   else if(hand2[0][1] == "Heart")    {
+                hand2i[0][1] = 3;
+            }   else if(hand2[0][1] == "Diamond")    {
+                hand2i[0][1] = 4;
+            }
+        }
+        else if(hand2[0][0] == "Jack")   {
+            hand2i[0][0] = 11;
+            if(hand2[0][1] == "Spade")  {
+                hand2i[0][1] = 1;
+            }   else if(hand2[0][1] == "Club")    {
+                hand2i[0][1] = 2;
+            }   else if(hand2[0][1] == "Heart")    {
+                hand2i[0][1] = 3;
+            }   else if(hand2[0][1] == "Diamond")    {
+                hand2i[0][1] = 4;
+            }
+        }
+        else if(hand2[0][0] == "Queen")   {
+            hand2i[0][0] = 12;
+            if(hand2[0][1] == "Spade")  {
+                hand2i[0][1] = 1;
+            }   else if(hand2[0][1] == "Club")    {
+                hand2i[0][1] = 2;
+            }   else if(hand2[0][1] == "Heart")    {
+                hand2i[0][1] = 3;
+            }   else if(hand2[0][1] == "Diamond")    {
+                hand2i[0][1] = 4;
+            }
+        }
+        else if(hand2[0][0] == "King")   {
+            hand2i[0][0] = 13;
+            if(hand2[0][1] == "Spade")  {
+                hand2i[0][1] = 1;
+            }   else if(hand2[0][1] == "Club")    {
+                hand2i[0][1] = 2;
+            }   else if(hand2[0][1] == "Heart")    {
+                hand2i[0][1] = 3;
+            }   else if(hand2[0][1] == "Diamond")    {
+                hand2i[0][1] = 4;
+            }
+        }
+        else if(hand2[0][0] == "Ace")   {
+            hand2i[0][0] = 1;
+            if(hand2[0][1] == "Spade")  {
+                hand2i[0][1] = 1;
+            }   else if(hand2[0][1] == "Club")    {
+                hand2i[0][1] = 2;
+            }   else if(hand2[0][1] == "Heart")    {
+                hand2i[0][1] = 3;
+            }   else if(hand2[0][1] == "Diamond")    {
+                hand2i[0][1] = 4;
+            }
+        }
+
+        if(hand2[1][0] == "2" || hand2[1][0] == "3" || hand2[1][0] == "4" || hand2[1][0] == "5" || hand2[1][0] == "6" || hand2[1][0] == "7" || hand2[1][0] == "8" || hand2[1][0] == "9" || hand2[1][0] == "10")  {
+            hand2i[1][0] = (int) hand2[1][0];
+            if(hand2[1][1] == "Spade")  {
+                hand2i[1][1] = 1;
+            }   else if(hand2[1][1] == "Club")    {
+                hand2i[1][1] = 2;
+            }   else if(hand2[1][1] == "Heart")    {
+                hand2i[1][1] = 3;
+            }   else if(hand2[1][1] == "Diamond")    {
+                hand2i[1][1] = 4;
+            }
+        }
+        else if(hand2[0][0] == "Jack")   {
+            hand2i[1][0] = 11;
+            if(hand2[1][1] == "Spade")  {
+                hand2i[1][1] = 1;
+            }   else if(hand2[1][1] == "Club")    {
+                hand2i[1][1] = 2;
+            }   else if(hand2[1][1] == "Heart")    {
+                hand2i[1][1] = 3;
+            }   else if(hand2[1][1] == "Diamond")    {
+                hand2i[1][1] = 4;
+            }
+        }
+        else if(hand2[0][0] == "Queen")   {
+            hand2i[1][0] = 12;
+            if(hand2[1][1] == "Spade")  {
+                hand2i[1][1] = 1;
+            }   else if(hand2[1][1] == "Club")    {
+                hand2i[1][1] = 2;
+            }   else if(hand2[1][1] == "Heart")    {
+                hand2i[1][1] = 3;
+            }   else if(hand2[1][1] == "Diamond")    {
+                hand2i[1][1] = 4;
+            }
+        }
+        else if(hand2[0][0] == "King")   {
+            hand2i[1][0] = 13;
+            if(hand2[1][1] == "Spade")  {
+                hand2i[1][1] = 1;
+            }   else if(hand2[1][1] == "Club")    {
+                hand2i[1][1] = 2;
+            }   else if(hand2[1][1] == "Heart")    {
+                hand2i[1][1] = 3;
+            }   else if(hand2[1][1] == "Diamond")    {
+                hand2i[1][1] = 4;
+            }
+        }
+        else if(hand2[0][0] == "Ace")   {
+            hand2i[1][0] = 1;
+            if(hand2[1][1] == "Spade")  {
+                hand2i[1][1] = 1;
+            }   else if(hand2[1][1] == "Club")    {
+                hand2i[1][1] = 2;
+            }   else if(hand2[1][1] == "Heart")    {
+                hand2i[1][1] = 3;
+            }   else if(hand2[1][1] == "Diamond")    {
+                hand2i[1][1] = 4;
+            }
+        }
+
          // removing the cards that are in the hand from the deck
-
-
+/*
+        int counter = 0;
         Object[] deck = deckOfCards.toArray();
         int n = deck.length;
         int combos = 5;
         String data[] = new String[combos];
         printCombination(deck, n, combos);
-
+*/
 
 
 
